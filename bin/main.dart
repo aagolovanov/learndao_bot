b55a57728243cf55fc92b613f715b1eb89c9d43f
Nov1X <angol_02@mail.ru>
2022-06-25 16:05:08 +0300

    base

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,32 @@
+import 'package:logging/logging.dart';
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
 
+import 'dart:io' show Platform; // for env
 
-Future<void> main() async {
-  const String token = "";
+
+void main() async {
+  final token = Platform.environment['TOKEN']!;
+
   final username = (await Telegram(token).getMe()).username;
 
   var teledart = TeleDart(token, Event(username!));
 
+
+  // registering events
   teledart.onMessage(keyword: "test")
     .listen((event) {event.reply("hello");});
 
 
-  teledart.start();
+  // Logging test
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message} // source: ${record.loggerName}');
+  });
+
+  final log = Logger("TestLogger");
+  log.fine("test log");
+  log.warning("test warning");
+
+  // teledart.start();
 }
